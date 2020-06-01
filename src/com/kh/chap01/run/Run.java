2436fc330329b9c2058399d69d6a841c16911a2e
osageu <dhwkdtjr123@naver.com>
2020-06-01 10:50:55 +0900

    ver 20.06.01_1.0.0

--- src/com/kh/chap01/run/Run.java
+++ src/com/kh/chap01/run/Run.java
@@ -1,10 +1,9 @@
 package com.kh.chap01.run;
-
+import com.kh.chap01.condition.*;
 public class Run {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
 
 }
