ae28efa36c6ce3c7387f2eb7f6ce30c582353f04
osageu <dhwkdtjr123@naver.com>
2020-06-04 14:15:12 +0900

    ver 20.06.04_1.0.0

---
+++ src/com/kh/array/A_Array.java
@@ -0,0 +1,8 @@
+package com.kh.array;
+
+public class A_Array {
+	 // 배열 : "같은 자료형의 값"들을 하나의 공간에 담을 수 있는 개념
+	// 				저장된 값마다 index가 지정됨 (index는 0부터 시작됨)
+	//
+	
+}
---
+++ src/com/kh/run/Run.java
@@ -0,0 +1,15 @@
+package com.kh.run;
+import com.kh.array.*;
+
+/**
+ * @author osageu
+ *
+ */
+public class Run {
+
+	public static void main(String[] args) {
+		A_Array osageu = new A_Array();
+	
+	}
+
+}
